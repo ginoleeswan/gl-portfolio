518cda76c8cb8dbd95f8ddf682b3e9442f4251e4
Gino Swanepoel <ginoleemusic@live.com>
2026-06-28 21:40:14 +0200

    feat: nav and footer with social links

---
+++ src/lib/social.ts
@@ -0,0 +1,7 @@
+export const EMAIL = "ginoleemusic@live.com";
+export const SOCIALS = [
+  { label: "GitHub", href: "https://github.com/ginoleeswan" },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/ginoswanepoel/" },
+  { label: "X", href: "https://twitter.com/MrGinoLee" },
+  { label: "StackOverflow", href: "https://stackoverflow.com/users/16642242/gino-swanepoel" },
+];
